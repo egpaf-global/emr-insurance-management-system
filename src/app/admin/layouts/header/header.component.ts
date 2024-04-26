264c0d57b8c898b795d432052d7a2455b5598e85
Emmanuel Z.K. Nyondo <emmanuelnyondo@gmail.com>
2024-04-26 13:10:58 +0200

    add: profile click toggle

--- src/app/admin/layouts/header/header.component.ts
+++ src/app/admin/layouts/header/header.component.ts
@@ -10,13 +10,17 @@ export class HeaderComponent {
   public userOne: string = Images.users.userOne;
   
   isOpen: boolean = false;
+  profileState: boolean = false;
 
   constructor(private element: ElementRef, private renderer: Renderer2) {}
 
   onClickProfile = () => {
+    let state = this.profileState ? 'false' : 'true';
+    this.profileState = !this.profileState
+    
     const profileDropdownList = this.element.nativeElement.querySelector(
       '.profile-dropdown-list'
     );
-    this.renderer.setAttribute(profileDropdownList, 'aria-expanded', 'true');
+    this.renderer.setAttribute(profileDropdownList, 'aria-expanded', state);
   };
 }
